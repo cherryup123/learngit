337c9d9f00f71c1b49e21596c00858a00081b85f
cherry <taoying@wealink.com>
2019-11-08 17:13:37 +0800

    stduy_line_1107

---
+++ aric/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+# @Time     :2019/11/8 0008 14:36
+# @Author   :Lemon_huahua
+#@File      :__init__.py.py
--- class1/class01_1.py
+++ class1/class01_1.py
@@ -1,4 +1,9 @@
 # -*- coding:utf-8 -*-
 # @Time     :2019/11/6 0006 15:54
 # @Author   :Lemon_huahua
-#@File      :class01_1.py
+#@File      :c01_1.py
+#九九乘法表
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(j,"*",i,"=",i*j,'','\t ',end='')
+    print("")
